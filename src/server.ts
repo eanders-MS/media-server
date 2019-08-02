b7242a2b4f2712aeefd065ec1587b8b560f583a1
Eric Anderson <eanders@microsoft.com>
2019-08-02 09:29:29 -0700

    simplify

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import RtspServer from 'rtsp-streaming-server';
 
-// Start media server
-const mediaServer = new RtspServer({
+process.on('uncaughtException', err => console.error('uncaughtException', err));
+process.on('unhandledRejection', reason => console.error('unhandledRejection', reason));
+
+new RtspServer({
     serverPort: 5554,
     clientPort: 6554,
     rtpPortStart: 10000,
     rtpPortCount: 10000
-});
-
-async function run() {
-    await mediaServer.start();
-}
-
-run().catch();
+}).start().catch();
